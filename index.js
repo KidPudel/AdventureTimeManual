db40cd0bb79e17ec7ebcf0f7791237fbe28628e3
Iggy Kuperson <i.kupchinenko@gmail.com>
2023-12-09 00:20:38 +0300

    js randomizing listener

---
+++ index.js
@@ -0,0 +1,25 @@
+console.log(document.title)
+
+const character = document.getElementsByClassName("character-name")[0]
+
+let clicked = 0;
+character.addEventListener("click", incrementOnClick) 
+
+function incrementOnClick() {
+    console.log(clicked);
+    character.innerHTML = (clicked++).toString() + "times clicked";
+}
+
+const characterBackground = document.getElementsByClassName("character")[0]
+let customColors = [];
+for (let i = 0; i < 100; i++) {
+    customColors.push("rgb(" + Math.floor(Math.random() * 256) + ", " +  Math.floor(Math.random() * 256) + ", " + Math.floor(Math.random() * 256) +  ") ")
+} 
+
+characterBackground.addEventListener("click", switchColor)
+
+function switchColor() {
+    const randomColor = customColors[Math.floor(Math.random() * 100)];
+    console.log(randomColor);
+    characterBackground.style.backgroundColor = randomColor;
+}
